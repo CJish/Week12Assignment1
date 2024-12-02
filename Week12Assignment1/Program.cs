84d4fb46b71592af2aa57422335f1a9b90365dc1
CJish <christopheru82@gmail.com>
2024-12-01 21:47:16 -0800

    Add project files.

---
+++ Week12Assignment1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+/*
+ * CSC180 Week12 Assignment #1
+ * key concepts: abstract class/method; inheritance; constructor; array of objects
+ * Tester: Program.cs
+ */
+using System;
+public class Program
+{
+    public static void Main()
+    {
+        Shape[] shapes = { new Square(10), new Circle(10), new Triangle(5,3), new Rectangle(10,4) }; // creates an array of Shapes with 2 objects inside of it: a square with sides of 10 and a circle with radius of 10
+        foreach (var shape in shapes) // goes through each shape in the shapes[] array
+        {
+            Console.WriteLine($"Area of {shape}: {shape.Area()}"); // outputs the area of the shape from 56
+            Console.WriteLine($"Area of {shape}: {Shape.GetArea(shape)}, again"); // uses the getter to print out the area of the shape (again)
+        }
+    }
+}
+
+
---
+++ Week12Assignment1/Shapes.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * CSC180 Week12 Assignment #1
+ * key concepts: abstract class/method; inheritance; constructor; array of objects
+ * Shape class: Shape.cs
+ */
+
+using System;
+
+public abstract class Shape
+{
+    public abstract double Area();
+    public static double GetArea(Shape shape)
+    {
+        return shape.Area();
+    }
+}
+
+public class Square : Shape
+{
+    private double size;
+    public Square(double length)
+    {
+        this.size = length;
+    }
+    public override double Area()
+    {
+        return Math.Pow(size, 2);
+    }
+}
+
+public class Circle : Shape
+{
+    private double radius;
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+    public override double Area()
+    {
+        return Math.Round(Math.PI * Math.Pow(radius, 2), 2);
+    }
+}
+
+public class Triangle : Shape
+{
+    private double triangleBase;
+    private double triangleHeight;
+
+    public Triangle(double b, double h)
+    {
+        this.triangleBase = b;
+        this.triangleHeight = h;
+    }
+
+    public override double Area()
+    {
+        return Math.Round((triangleBase * triangleHeight) / 2, 2);
+    }
+}
+
+public class Rectangle : Shape
+{
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width)
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+    public override double Area()
+    {
+        return Math.Round(length * width, 2);
+    }
+}
